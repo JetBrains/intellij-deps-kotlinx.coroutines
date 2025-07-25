e3db17cf9380f0eabae512f73b4c6042746290e7
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-07-25 11:51:27 +0200

    Fix IntelliJ API

--- kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
+++ kotlinx-coroutines-core/jvm/src/internal/intellij/intellij.kt
@@ -3,8 +3,15 @@
  */
 package kotlinx.coroutines.internal.intellij
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlin.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+import kotlin.jvm.Throws
+import kotlin.time.Duration
 
 internal val currentContextThreadLocal : ThreadLocal<CoroutineContext?> = ThreadLocal.withInitial { null }
 
@@ -24,5 +31,51 @@ public object IntellijCoroutines {
      */
     public fun currentThreadCoroutineContext(): CoroutineContext? {
         return currentContextThreadLocal.get()
+    }
+
+
+    /**
+     * An analogue of [runBlocking][kotlinx.coroutines.runBlocking] that [compensates parallelism][kotlinx.coroutines.scheduling.withCompensatedParallelism]
+     * while the coroutine is not complete and the associated event loop has no immediate work available.
+     */
+    @Throws(InterruptedException::class)
+    public fun <T> runBlockingWithParallelismCompensation(
+        context: CoroutineContext,
+        block: suspend CoroutineScope.() -> T
+    ): T = runBlocking(context, block)
+
+    /**
+     * Constructs a [SoftLimitedDispatcher] from the specified [CoroutineDispatcher].
+     * [SoftLimitedDispatcher] behaves as [LimitedDispatcher][kotlinx.coroutines.internal.LimitedDispatcher] but allows
+     * temporarily exceeding the parallelism limit in case [parallelism compensation][kotlinx.coroutines.scheduling.withCompensatedParallelism]
+     * was requested (e.g., by [kotlinx.coroutines.runBlocking]).
+     *
+     * This extension can only be used on instances of [Dispatchers.Default], [Dispatchers.IO] and also on what this extension
+     * has returned. Throws [UnsupportedOperationException] if [this] does not support the parallelism compensation mechanism.
+     */
+    public fun CoroutineDispatcher.softLimitedParallelism(parallelism: Int, name: String?): CoroutineDispatcher =
+        limitedParallelism(parallelism, name)
+
+    @Deprecated("use named version", level = DeprecationLevel.HIDDEN)
+    public fun CoroutineDispatcher.softLimitedParallelism(parallelism: Int): CoroutineDispatcher =
+        limitedParallelism(parallelism, null)
+
+    /**
+     * Executes [action] and **advises** to compensate parallelism if [action] does not finish within [timeout].
+     *
+     * Suppose that you are dealing with an operation that blocks the underlying thread.
+     * If the blocked thread happens to be from a limited-thread-pool dispatcher, then you face the problem of thread starvation:
+     * your system underutilizes the available CPU. In severe cases, this may result in a deadlock.
+     * To increase the size of the thread pool, you can wrap the operation into this function.
+     *
+     * Remember that threads are expensive, so you need to choose an appropriate [timeout]
+     * so that parallelism compensation would be less expensive than waiting for [action] to finish.
+     *
+     * There are intentionally few guarantees that this function will have side effects.
+     * Generally, parallelism compensation applies only to threads that are ready for compensation,
+     * such as [Dispatchers.Default] or [Dispatchers.IO].
+     */
+    public fun <T> runAndCompensateParallelism(timeout: Duration, action: () -> T): T {
+        return action()
     }
 }
